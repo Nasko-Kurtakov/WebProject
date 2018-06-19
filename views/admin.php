9be7bb12f8601855aa918ea8f3e2345efa18fba5
aleks <aleks.gospodinova@gmail.com>
2018-06-20 02:18:18 +0300

    added an img folder

---
+++ views/admin.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asxelag
+ * Date: 20-Jun-18
+ * Time: 1:24 AM
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../styles/styleAdmin.css" rel="stylesheet" />
+</head>
+<body>
+
+<div class="view-container">
+    <h1> Добре дошъл ...</h1>
+
+<div class="row">
+  <div class="column" style="background-color:#aaa;">
+      <div class="container">
+          <h2>Създай</h2>
+          <p>Some text..</p>
+      </div>
+  </div>
+  <div class="column" style="background-color:#bbb;">
+      <div class="container">
+          <h2>Оцени</h2>
+          <p>Some text..</p>
+      </div>
+  </div>
+  <div class="column" style="background-color:#ccc;">
+      <div class="container">
+          <h2>Прикачи</h2>
+          <p>Some text..</p>
+      </div>
+  </div>
+</div>
+</div>
+
+</body>
+</html>

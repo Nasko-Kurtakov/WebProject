b844b2a626d671f4bcde6271876590fd5d015d75
aleks <aleks.gospodinova@gmail.com>
2018-06-30 01:31:34 +0300

    upload errors

--- views/uploadTests.php
+++ views/uploadTests.php
@@ -31,13 +31,17 @@ if (isset($_SESSION["user"]) && $_SESSION["user"]) {
     <title>Качи тестове</title>
 </head>
 <body>
-<div class="w3-bar w3-dark-grey w3-border w3-large" >
+<div class="w3-bar w3-dark-grey w3-large">
     <a href="<?php echo $mainPage?>" class="w3-bar-item w3-button homeButton"><i class="fa fa-home"></i></a>
     <button class="w3-bar-item w3-button w3-mobile send-btn" data-bind="click:function(){
                     $data.sendTests();
             }">Качи файловете</button>
     <span class="w3-bar-item" data-bind=""></span>
     <span class="w3-bar-item success" data-bind=""></span>
+</div>
+<div class="w3-dark-gray err-success-bar text-center">
+    <span class="w3-bar-item" data-bind="text:$data.error"></span>
+    <span class="w3-bar-item success" data-bind="text:$data.success"></span>
 </div>
 <div class="container">
     <div class="text-center choose-test">
